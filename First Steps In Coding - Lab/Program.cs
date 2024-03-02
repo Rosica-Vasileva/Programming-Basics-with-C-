4741aa5d5a9e53d315a616a560bfd7ebc4e0988a
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-03-02 15:51:16 +0200

    02. Nums 1...10

--- First Steps In Coding - Lab/Program.cs
+++ First Steps In Coding - Lab/Program.cs
@@ -1,8 +1,13 @@
 ﻿
 
-Console.Write("Hello SoftUni");
-Console.WriteLine("Hello SoftUni");
-Console.WriteLine("Hello SoftUni");
-Console.Write("Hello Rosi");
-
+Console.WriteLine(1);
+Console.WriteLine(2);
+Console.WriteLine(3);
+Console.WriteLine(4);
+Console.WriteLine(5);
+Console.WriteLine(6);
+Console.WriteLine(7);
+Console.WriteLine(8);
+Console.WriteLine(9);
+Console.WriteLine(10);
 
