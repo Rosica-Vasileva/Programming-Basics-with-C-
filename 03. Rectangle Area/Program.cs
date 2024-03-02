4a95460d1d8355c11b6c8962ad7ee6ba925bf3e0
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-03-02 21:33:04 +0200

    Add files via upload

---
+++ 03. Rectangle Area/Program.cs
@@ -0,0 +1,8 @@
+﻿
+//Entered name from Rossi console
+string name = Console.ReadLine();
+Console.WriteLine(name);
+
+//Here we change the value of the name variable to "Rosica".
+name = "Rosica";
+Console.WriteLine(name);
