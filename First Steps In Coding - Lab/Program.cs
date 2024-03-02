6678702b4c3fc9f600261e528df7bf1bea4bbeea
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-03-02 15:51:58 +0200

    Add files via upload

--- First Steps In Coding - Lab/Program.cs
+++ First Steps In Coding - Lab/Program.cs
@@ -1,13 +1,8 @@
 ﻿
 
-Console.WriteLine(1);
-Console.WriteLine(2);
-Console.WriteLine(3);
-Console.WriteLine(4);
-Console.WriteLine(5);
-Console.WriteLine(6);
-Console.WriteLine(7);
-Console.WriteLine(8);
-Console.WriteLine(9);
-Console.WriteLine(10);
+Console.Write("Hello SoftUni");
+Console.WriteLine("Hello SoftUni");
+Console.WriteLine("Hello SoftUni");
+Console.Write("Hello Rosi");
+
 
