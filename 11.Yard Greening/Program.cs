7dd87283f05da5214812295898bdc406becc7447
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-03-03 13:04:43 +0200

    Add files via upload

---
+++ 11.Yard Greening/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+//5
+int dogFood = int.Parse(Console.ReadLine());
+//4
+int catFood = int.Parse(Console.ReadLine());
+
+double expenses = dogFood * 2.5 + catFood * 4;
+Console.WriteLine($"{expenses}lv.");
+//28.5
