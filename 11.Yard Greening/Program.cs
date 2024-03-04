b31c49d417b155fc24f98397d7e99319f0f3f0c5
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-03-04 12:47:01 +0200

    Update Program.cs

--- 11.Yard Greening/Program.cs
+++ 11.Yard Greening/Program.cs
@@ -1,10 +1,18 @@
-﻿
+Input     Output
+550lv.     final price 3432.11 lv.
+           discount 753.39lv
+  
+Input     Output  
+150lv.     final price 936.03 lv.
+           discount 205.47v
 
-//5
-int dogFood = int.Parse(Console.ReadLine());
-//4
-int catFood = int.Parse(Console.ReadLine());
 
-double expenses = dogFood * 2.5 + catFood * 4;
-Console.WriteLine($"{expenses}lv.");
-//28.5
+double yardArea = double.Parse(Console.ReadLine());
+
+double price = yardArea * 7.61;
+
+double discount = price * 0.18;
+double finalPrice = price - discount;
+
+Console.WriteLine($"The final price is: {finalPrice} lv.");
+Console.WriteLine($"The discount is: {discount} lv.");
