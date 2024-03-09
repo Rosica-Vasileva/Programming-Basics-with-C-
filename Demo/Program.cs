56c82e4a0f02a7d80c9f6f0b9bb467333f2ae273
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-03-09 23:19:18 +0200

    Add files via upload

---
+++ Demo/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+//π=Math.Pi
+//degrees=radians*180 / π
+//INPUT:3.1416 OUTPUT:180.0004209182994 INPUT:6.2832 OUTPUT:360.0008418365988 INPUT:0.7854 OUTPUT:45.00010522957485
+double radians = double.Parse(Console.ReadLine());
+double degrees = radians * 180 / Math.PI;
+Console.WriteLine(degrees);
+
+
