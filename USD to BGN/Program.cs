5fb6bbed138f95d1eac5a9acebcfce0f07e22dbf
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-03-09 23:26:21 +0200

    Add files via upload

---
+++ USD to BGN/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+
+//1 USD 1.79549 BGN
+//INPUT; 22 ,100 , 12.5
+//OUTPUT; 39.50078 , 179.549 , 22.443625
+
+
+double usd = double.Parse(Console.ReadLine());
+double bgn = usd * 1.79549;
+Console.WriteLine(bgn);
+
