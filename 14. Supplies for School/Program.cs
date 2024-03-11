1f951fb0c9fce2be088f5880c57280c1a55912f1
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-03-12 00:44:54 +0200

    Add files via upload

---
+++ 14. Supplies for School/Program.cs
@@ -0,0 +1,26 @@
+﻿
+//INPUT   //OUTPUT
+//2       //28.5
+//3 
+//4 
+//25       
+
+
+        double packOfPen = double.Parse(Console.ReadLine());
+        double packOfMarker = double.Parse(Console.ReadLine());
+        double cleanLitries = double.Parse(Console.ReadLine());
+        double discount = double.Parse(Console.ReadLine());
+
+        double penPrice = packOfPen * 5.80; // Коригирано: packOfPen, а не packOfMarker
+        double markerPrice = packOfMarker * 7.20;
+        double cleanPrice = cleanLitries * 1.20;
+
+        double allPrice = penPrice + markerPrice + cleanPrice;
+        double discountPrice = allPrice - (allPrice * discount) / 100;
+
+        Console.WriteLine($"Annie will need {discountPrice} money.");
+
+
+
+
+
