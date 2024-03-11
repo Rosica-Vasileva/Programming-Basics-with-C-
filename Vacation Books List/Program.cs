510c18d397905b52f3979b3160392dc6d865f134
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-03-11 23:34:24 +0200

    Add files via upload

---
+++ Vacation Books List/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+//INPUT   //OUTPUT    //INPUT  //OUTPUT
+//212    //7         //432     //7
+//20                //15
+//2                //4
+
+
+int pageNum = int.Parse(Console.ReadLine());
+int pageOneHours = int.Parse(Console.ReadLine());
+int dayNum = int.Parse(Console.ReadLine());
+
+int totalHours = pageNum / pageOneHours;
+int hourseDay = totalHours / dayNum;
+Console.WriteLine($"{hourseDay}");
