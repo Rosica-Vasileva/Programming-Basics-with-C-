6b07a0e35048cfa506f976434389018f9476f9c7
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-03-11 22:48:37 +0200

    Update Program.cs

--- 08.Concatenate Data/Program.cs
+++ 08.Concatenate Data/Program.cs
@@ -1,8 +1,12 @@
 ﻿
 
 
+    //INPUT     //OUTPUT
+   // Rosica    //"You are Rosica Vasileva, a 32 -years old person from Sofia."
+  //Vasileva 
+ //32 
+//Sofia
 
-// Rosica Vasileva 32 Sofia
 string firstName = Console.ReadLine();
 string lastName = Console.ReadLine();
 int age = int.Parse(Console.ReadLine()); 
