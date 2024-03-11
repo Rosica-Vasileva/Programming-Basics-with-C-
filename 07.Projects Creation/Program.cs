8e9ccdc414fa4130ff3bb3f3cb213342da7126d1
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-03-11 22:51:17 +0200

    Update and rename Program.cs to Program.cs

--- 07.Projects Creation/Program.cs
+++ 07.Projects Creation/Program.cs
@@ -1,5 +1,8 @@
 ﻿
-
+//INPUT  //OUTPUT
+//4      //The architect Mihail will need 12 hours to complete 4 project/s.
+//9
+//Mihail
 string name = Console.ReadLine();
 int numberOfProjects = int.Parse(Console.ReadLine());
 
