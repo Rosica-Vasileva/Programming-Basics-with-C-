14019627bda290cbff446cbe2b0ec9a91c007af4
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-03-12 23:53:16 +0200

    Add files via upload

---
+++ 17.Basketball Equipment/Program.cs
@@ -0,0 +1,16 @@
+﻿
+      
+
+
+int yearlyFee = int.Parse(Console.ReadLine());
+
+        double basketballShoesPrice = yearlyFee - (yearlyFee * 0.40);
+        double basketballKitPrice = basketballShoesPrice - (basketballShoesPrice * 0.20);
+        double basketballBallPrice = basketballKitPrice / 4;
+        double basketballAccessoriesPrice = basketballBallPrice / 5;
+
+        double totalCost = yearlyFee + basketballShoesPrice + basketballKitPrice + basketballBallPrice + basketballAccessoriesPrice;
+
+        Console.WriteLine($"{totalCost:F2}");
+
+
