e56fcea9cc6a861cd6e12b6d2f2cd16137119816
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-03-12 23:34:47 +0200

    Add files via upload

---
+++ 15.Repainting/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        double nylonKvm = double.Parse(Console.ReadLine());
+        double paintLitres = double.Parse(Console.ReadLine());
+        double thinnerLitres = double.Parse(Console.ReadLine());
+        double hourse = double.Parse(Console.ReadLine());
+
+        double nylonPrice = (nylonKvm + 2) * 1.50;
+        double paintPrice = (paintLitres + (paintLitres * 0.10)) * 14.50;
+        double thinnerPrice = thinnerLitres * 5.00;
+        double bag = 0.40;
+
+        double totalPrice = nylonPrice + paintPrice + thinnerPrice + bag;
+        double masterPrice = (totalPrice * 0.30) * hourse;
+        double finalPrice = totalPrice + masterPrice;
+
+        Console.WriteLine(finalPrice);
+    }
+}
