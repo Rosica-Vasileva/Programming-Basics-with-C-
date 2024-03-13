75d41ee0277eb227f19d3c8eea7f86e38fa202d0
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-03-13 07:59:03 +0200

    Add files via upload

---
+++ 18.Fish Tank/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+
+
+int length = int.Parse(Console.ReadLine());
+double width = double.Parse(Console.ReadLine());
+double height = double.Parse(Console.ReadLine());
+double percentage = double.Parse(Console.ReadLine());
+
+int volume = (int)(length * width * height);
+double volumeLitres = volume / 1000.0;
+double occupiedSpace = percentage / 100.0;
+double litresNeeded = volumeLitres * (1 - occupiedSpace);
+Console.WriteLine($"{litresNeeded:F3}");
