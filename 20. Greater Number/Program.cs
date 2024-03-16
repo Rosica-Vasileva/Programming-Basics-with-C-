00ddafe97949ccd2413420755f21a731c90da9ad
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-03-16 22:39:08 +0200

    Add files via upload

---
+++ 20. Greater Number/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+//5  //6   //2  //9
+//3  //1   //2   //-14.5
+
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 > num2)
+    Console.WriteLine(num1);
+else (num1< num2)
+    Console.WriteLine(num2);
