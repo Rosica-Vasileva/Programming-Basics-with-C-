0bf7a54570adee9e4ebb36f75098506fbb95dd9f
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-03-18 13:35:05 +0200

    Add files via upload

---
+++ 23.Password Guess/Program.cs
@@ -0,0 +1,19 @@
+﻿
+//INPUT
+//s3cr3t!P@ssw0rd
+string password = Console.ReadLine();
+
+       
+        if (password == "s3cr3t!P@ssw0rd")
+        {
+            Console.WriteLine("Welcome");
+        }
+        else
+        {
+            Console.WriteLine("Wrong password!");
+        }
+
+
+
+
+
