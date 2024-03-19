797e222ded3689beb8cce69a6a804068352a3875
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-03-19 14:44:13 +0200

    Add files via upload

---
+++ 23.Area of Figures/Program.cs
@@ -0,0 +1,30 @@
+﻿
+        string shape = Console.ReadLine();
+        double area;
+
+        if (shape == "square")
+        {
+            double side = double.Parse(Console.ReadLine());
+            area = side * side;
+        }
+        else if (shape == "rectangle")
+        {
+            double sideA = double.Parse(Console.ReadLine());
+            double sideB = double.Parse(Console.ReadLine());
+            area = sideA * sideB;
+        }
+        else if (shape == "circle")
+        {
+            double r = double.Parse(Console.ReadLine());
+            area = Math.PI * r * r;
+        }
+        else 
+        {
+            double a = double.Parse(Console.ReadLine());
+            double hA = double.Parse(Console.ReadLine());
+            area = a * hA / 2;
+        }
+        
+
+        Console.WriteLine($"{area:F3}");
+ 
