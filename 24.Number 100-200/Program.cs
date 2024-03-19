62f8ec648c948a5c4d9aea7a1550066ed1323d83
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-03-19 15:17:40 +0200

    Add files via upload

---
+++ 24.Number 100-200/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+//To write the program that reads the whole number entered by the user and checks if it is less than 100, between 100 and 200 or more than 200. If the number is:
+//· under 100 print: "Less than 100"
+//· between 100 and 200 print: "Between 100 and 200"
+//· over 200 print: "More than 200"
+
+int number = int.Parse(Console.ReadLine());
+
+if (number < 100)
+{
+    Console.WriteLine("Less than 100");
+}
+
+else if (number <=200)
+{
+    Console.WriteLine("Between 100-200");
+
+}
+
+else
+{
+    Console.WriteLine("Greater than 200");
+}
