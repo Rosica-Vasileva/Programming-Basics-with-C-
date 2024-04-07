87e90f8e1ed4c7e62b4cc099dffa8562c497b619
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-04-07 23:43:42 +0300

    Add files via upload

---
+++ 34.Day of Week/Program.cs
@@ -0,0 +1,37 @@
+﻿//1 "Monday"
+//2 "Tuesday"
+//3 "Wednesday"
+//4 "Thursday"
+//5 "Friday"
+//6 "Saturday"
+//7 "Sunday"
+//-1 "Error"
+
+int dayOfWeek = int.Parse(Console.ReadLine());
+switch (dayOfWeek)
+{
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+    case 4:
+        Console.WriteLine("Thursday");
+        break;
+    case 5:
+        Console.WriteLine("Friday");
+        break;
+    case 6:
+        Console.WriteLine("Saturday");
+        break;
+    case 7:
+        Console.WriteLine("Sunday");
+        break;
+   default:
+        Console.WriteLine("Error");
+        break;
+}
