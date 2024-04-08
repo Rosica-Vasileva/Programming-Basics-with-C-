26be9a0dee07049896b39c5d2dd77f40c87b6579
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-04-08 23:54:21 +0300

    Add files via upload

---
+++ 36.Fruit or vegetable/Program.cs
@@ -0,0 +1,26 @@
+﻿//banana fruit
+//apple fruit
+//tomato vegetable
+//water unknown
+
+string input = Console.ReadLine();
+switch (input)
+{
+    case "banana":
+    case "apple":
+    case "kiwi":
+    case "cherry":
+    case "lemon":
+    case "grapes":
+        Console.WriteLine("fruit");
+        break;
+    case "tomato":
+    case "cucumber":
+    case "pepper":
+    case "carot":
+        Console.WriteLine("vegetable");
+        break;
+    default:
+        Console.WriteLine("unknow");
+        break;        
+}
