11ffc2f7862cb142dec41ea23090ff6acd5a4498
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-04-09 00:29:11 +0300

    Add files via upload

---
+++ 37. Personal Titles/Program.cs
@@ -0,0 +1,41 @@
+﻿//12 f Miss
+//17 m Mr.
+//25 f Ms.
+//13.5 m Master
+
+using System;
+
+
+        Console.WriteLine("Enter age:");
+        double age = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter gender (m/f):");
+        char gender = char.Parse(Console.ReadLine());
+
+        if (gender == 'f')
+        {
+            if (age < 16)
+            {
+                Console.WriteLine("Miss");
+            }
+            else
+            {
+                Console.WriteLine("Ms");
+            }
+        }
+        else if (gender == 'm')
+        {
+            if (age < 16)
+            {
+                Console.WriteLine("Master");
+            }
+            else
+            {
+                Console.WriteLine("Mr");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid gender input.");
+        }
+
