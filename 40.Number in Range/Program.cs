8ba01535af3da508e497d6073bc45bb235f82f5e
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-04-11 00:09:36 +0300

    Add files via upload

---
+++ 40.Number in Range/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+//-25 Yes
+//0 No
+//25 Yes
+
+
+int number = int.Parse(Console.ReadLine());
+
+if (number != 0 && number >= -100 && number <= 100)
+{
+    Console.WriteLine("Yes");
+}
+
+else if (number!=0)
+{
+    Console.WriteLine("No");    
+}
