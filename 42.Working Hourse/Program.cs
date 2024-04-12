8968304abcd7097b9df46e885959c294779c3069
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-04-12 13:32:15 +0300

    Add files via upload

---
+++ 42.Working Hourse/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System.ComponentModel.Design;
+
+int hour = int.Parse(Console.ReadLine());
+string day = Console.ReadLine();
+bool isOpen = false;
+
+switch (day)
+{
+    case "monday":
+    case "tuesday":
+    case "wednesday":
+    case "thursday":
+    case "friday":
+    case "saturday":
+        if (hour >= 10 && hour <= 18)
+        {
+            Console.WriteLine("Open");
+        }
+        break;
+
+
+        else
+        {
+            Console.WriteLine("Close");
+        }
+        break;
