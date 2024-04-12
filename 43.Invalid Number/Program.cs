4748c503d0804cb2ac38503fdd6200da7fb7e0ab
Rosica-Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-04-12 13:48:36 +0300

    Add files via upload

---
+++ 43.Invalid Number/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+//75 invalid
+//150 nowayout
+//220 invalid
+//199 nowayout
+//-1 invalid
+//100 nowayout
+//200 nowayout
+//0 nowayout
+
+int number = int.Parse(Console.ReadLine());
+if (number >= 100 && number <= 200 || number == 0)
+{
+    Console.WriteLine("valid");
+}
+else
+{
+    Console.WriteLine("invalid");
+}
+
