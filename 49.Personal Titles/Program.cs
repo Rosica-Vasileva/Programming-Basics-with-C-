29c128412e7d19215374547fb7226a236fe7947c
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-04-18 15:24:22 +0300

    Add files via upload

---
+++ 49.Personal Titles/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+int age = int.Parse(Console.ReadLine());
+char gender = char.Parse(Console.ReadLine());
+
+if (gender == 'f')
+{
+    if (age < 16)
+    {
+        Console.WriteLine("Miss");
+    }
+    else // age >= 16
+    {
+        Console.WriteLine("Ms.");
+    }
+}
+else // gender == 'm'
+{
+    if (age < 16)
+    {
+        Console.WriteLine("Master");
+    }
+    else // age >= 16
+    {
+        Console.WriteLine("Mr");
+    }
+}
