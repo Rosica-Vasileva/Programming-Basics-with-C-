291dc1e258937847035de7f2177498d0e4bfe1a8
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-04-18 13:07:18 +0300

    Add files via upload

---
+++ 46.Task with switch ,if,else if,else construct/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+int day = int.Parse(Console.ReadLine());
+string stringDay;
+
+switch(day)
+{
+    case 1:
+        stringDay = "Monday";
+        break;
+    case 2:
+        stringDay = "Tuesday";
+        break;
+    case 3:
+        stringDay = "Wednesday";
+        break;
+    case 4:
+        stringDay = "Thurdsday";
+        break;
+    case 5:
+        stringDay = "Friday";
+        break;
+    case 6:
+        stringDay = "Saturday";
+        break;
+    case 7:
+        stringDay = "Sunday";
+        break;
+    default:
+        stringDay = "Unknow";
+        break;
+
+}
+Console.WriteLine("Today is" + stringDay);
