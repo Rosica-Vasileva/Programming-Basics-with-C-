752f4d83d731621cc41bebbc15ba17a1c3a1b29e
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-04-21 23:00:41 +0300

    Add files via upload

---
+++ 54.Cinema/Program.cs
@@ -0,0 +1,33 @@
+﻿
+        
+        
+        
+
+string projectionType = Console.ReadLine();
+int rows = int.Parse(Console.ReadLine());
+int cols = int.Parse(Console.ReadLine());
+
+        double income = 0;
+        int totalSeats = rows * cols;
+
+        
+        switch (projectionType)
+        {
+            case "Premiere":
+                income = totalSeats * 12.00;
+                break;
+            case "Normal":
+                income = totalSeats * 7.50;
+                break;
+            case "Discount":
+                income = totalSeats * 5.00;
+                break;
+            default:
+                Console.WriteLine("Error!");
+                return;
+        }
+
+        
+        Console.WriteLine("{0:f2} leva", income);
+ 
+
