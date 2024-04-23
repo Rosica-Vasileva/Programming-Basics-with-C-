05836c9ac3f5077686a5c8227343f262f6039e07
Rosica Vasileva <150438254+Rosica-Vasileva@users.noreply.github.com>
2024-04-24 00:36:01 +0300

    Add files via upload

---
+++ 59.Concatenate Data/Program.cs
@@ -0,0 +1,61 @@
+﻿
+
+        // Четене на входните данни от конзолата
+        int N1 = int.Parse(Console.ReadLine());
+        int N2 = int.Parse(Console.ReadLine());
+        char operation = char.Parse(Console.ReadLine());
+
+        // Променлива за съхранение на резултата от операцията
+        double result = 0;
+
+        // Извършване на математическата операция
+        switch (operation)
+        {
+            case '+':
+                result = N1 + N2;
+                Console.WriteLine($"{N1} + {N2} = {result} - {IsEven(result)}");
+                break;
+            case '-':
+                result = N1 - N2;
+                Console.WriteLine($"{N1} - {N2} = {result} - {IsEven(result)}");
+                break;
+            case '*':
+                result = N1 * N2;
+                Console.WriteLine($"{N1} * {N2} = {result} - {IsEven(result)}");
+                break;
+            case '/':
+                if (N2 == 0)
+                {
+                    Console.WriteLine($"Cannot divide {N1} by zero");
+                }
+                else
+                {
+                    result = (double)N1 / N2;
+                    Console.WriteLine($"{N1} / {N2} = {result:f2}");
+                }
+                break;
+            case '%':
+                if (N2 == 0)
+                {
+                    Console.WriteLine($"Cannot divide {N1} by zero");
+                }
+                else
+                {
+                    result = N1 % N2;
+                    Console.WriteLine($"{N1} % {N2} = {result}");
+                }
+                break;
+        }
+   
+
+    // Метод за определяне дали число е четно или нечетно
+    static string IsEven(double number)
+    {
+        return number % 2 == 0 ? "even" : "odd";
+    }
+
+
+
+
+
+
